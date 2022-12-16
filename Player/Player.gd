df9c2e53a59521f69ab925add2a0eadc23252589
Alexander Mackey <alxmke@berkeley.edu>
2022-12-16 01:58:32 -0500

    added camera follow refactored jump code

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,7 +4,8 @@ export var ACCELERATION: float = 512
 export var SPEED: float = 64
 export var FRICTION: float = 512
 export var GRAVITY: float = 300
-export var JUMP_FORCE: float = 128
+export var JUMP_FORCE: float = -128
+export var SMALL_JUMP: float = JUMP_FORCE * 0.5
 export var MAX_SLOPE_ANGLE = 46
 
 var motion = Vector2.ZERO
@@ -17,19 +18,20 @@ func _ready(): pass
 
 func _physics_process(delta):
 	var x: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
 	var on_floor: bool = is_on_floor()
-	
-	if x != 0 and on_floor:
-		motion.x = move_toward(motion.x, x * SPEED, ACCELERATION * delta)
-	elif on_floor:
-		motion.x = move_toward(motion.x, 0, FRICTION * delta)
-	elif x != 0:
-		motion.x = move_toward(motion.x, x * SPEED, ACCELERATION/10 * delta)
+	if on_floor:
+		if x:
+			motion.x = move_toward(motion.x, x * SPEED, ACCELERATION * delta)
+		else:
+			motion.x = move_toward(motion.x, 0, FRICTION * delta)
+	elif x:
+		motion.x = move_toward(motion.x, x * SPEED, ACCELERATION * 0.1 * delta)
 	
 	if on_floor and Input.is_action_just_pressed("ui_up"):
-		motion.y = -JUMP_FORCE
-	elif Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
-		motion.y = -JUMP_FORCE/2
+		motion.y = JUMP_FORCE
+	elif motion.y < SMALL_JUMP and not Input.is_action_pressed("ui_up"):
+		motion.y = SMALL_JUMP
 	else:
 		motion.y += GRAVITY * delta
 	
