7beed7ddea2754176049192d018f98308cf824ec
Alexander Mackey <alxmke@berkeley.edu>
2022-12-19 14:59:43 -0500

    added angular tiles (some to the world) added mild coyote jump

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,11 +8,13 @@ export var JUMP_FORCE: float = -150
 export var SMALL_JUMP: float = JUMP_FORCE * 0.5
 export var MAX_SLOPE_ANGLE: float = deg2rad(46)
 
-var motion = Vector2.ZERO
-var snap_vector = Vector2.DOWN
+var motion: Vector2 = Vector2.ZERO
+var snap_vector: Vector2 = Vector2.DOWN
+var can_coyote_jump: bool = false
 
 onready var sprite = $Sprite
 onready var animation_player = $AnimationPlayer
+onready var coyote_jump = $CoyoteJump
 
 # Called when the node enters the scene tree for the first time.
 func _ready(): pass
@@ -26,6 +28,7 @@ func _physics_process(delta):
 	move()
 
 func move():
+	var was_on_floor: bool = is_on_floor()
 	motion = move_and_slide_with_snap(
 		motion, # linear_velocity
 		snap_vector * 4, # snap
@@ -35,12 +38,17 @@ func move():
 		MAX_SLOPE_ANGLE # floor_max_angle: float = 0.785398,
 		# infinite_inertia: bool = true)
 	)
+	
+	if was_on_floor and not is_on_floor():
+		can_coyote_jump = true
+		coyote_jump.start()
 
 # applies gravity or jump force
 func jump(delta):
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or can_coyote_jump):
 		snap_vector = Vector2.ZERO
 		motion.y = JUMP_FORCE
+		can_coyote_jump = false
 	elif Input.is_action_just_released("ui_up") and motion.y < SMALL_JUMP:
 		motion.y = SMALL_JUMP
 	else:
@@ -67,3 +75,7 @@ func animate(x):
 	
 	if not is_on_floor():
 		animation_player.play("jump")
+
+
+func _on_CoyoteJump_timeout():
+	can_coyote_jump = false
