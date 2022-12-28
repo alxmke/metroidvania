9ac8a3db13b3ac11878227d9f5f01cfe9454f388
Alexander Mackey <alxmke@berkeley.edu>
2022-12-27 22:23:43 -0500

    added enemy base class added fun little walking snail guy

---
+++ Enemies/Enemy.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+export var BASE_SPEED: float = 15
+
+var motion: Vector2 = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready(): pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta): pass
