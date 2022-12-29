cd662dfc1f50d92a6b6ebf933607d2a12ab1e4ec
Alex Mackey <alxmke@berkeley.edu>
2022-12-29 10:57:39 -0500

    bullets collide with enemies and environment

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -14,3 +14,9 @@ func _physics_process(delta):
 
 func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
+
+func _on_Hitbox_body_entered(_body):
+	queue_free()
+
+func _on_Hitbox_area_entered(_area):
+	queue_free()
