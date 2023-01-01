6eb5e9153d823aed8c6323603a86663c4a0c3aa9
Alexander Mackey <alxmke@berkeley.edu>
2023-01-01 12:19:03 -0500

    refactored enemy classes

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -4,12 +4,16 @@ export var BASE_SPEED: float = 15
 
 var motion: Vector2 = Vector2.ZERO
 
+onready var enemy_stats = $EnemyStats
+
 # Called when the node enters the scene tree for the first time.
 func _ready(): pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta): pass
 
-
 func _on_Hurtbox_got_hurt(damage):
+	enemy_stats.health -= damage
+
+func _on_EnemyStats_died():
 	queue_free()
